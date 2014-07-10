3b4effa5cbef3112df7781cdb92db5cfe8ec003c
bmitch <wkmitch@gmail.com>
2014-07-09 20:51:23 -0400

    After episode 9

--- app/config/app.php
+++ app/config/app.php
@@ -78,7 +78,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => '02gC8btKbG7eAoblIo5TLUxpUUkBnN08',
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
@@ -122,7 +122,8 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
-		'Laracasts\Commander\CommanderServiceProvider'
+		'Laracasts\Commander\CommanderServiceProvider',
+		'Laracasts\Validation\ValidationServiceProvider'
 
 	),
 
--- app/controllers/RegistrationController.php
+++ app/controllers/RegistrationController.php
@@ -1,8 +1,15 @@
 <?php
 
+use Larabook\Forms\RegistrationForm;
+
 class RegistrationController extends \BaseController {
 
+	private $reggistrationForm;
 
+	function __construct(RegistrationForm $registrationForm)
+	{
+		$this->registrationForm = $registrationForm;
+	}
 	// Show a form to register the user
 	public function create()
 	{
@@ -12,6 +19,9 @@ class RegistrationController extends \BaseController {
 	// Create new user
 	public function store()
 	{
+
+		$this->registrationForm->validate(Input::all());
+
 		$user = User::create(
 			Input::only('username', 'email', 'password')
 		);
